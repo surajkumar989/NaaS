e589cf87cd200246ceb713bf33ac282b052d83bc
Suraj Kumar <maybesurajkumar@gmail.com>
2026-07-22 00:59:43 +0530

    setup corrected

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
 app.get('/', (req, res) => {
     res.send(`
         <h1> Hello World</h1>
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import express from'express'
+import app from './app.js'
 
-await connectDB();
-
-const app = express();
 const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-    res.send(`
-        <h1> Hello World</h1>
-        ${process.env.PORT}
-        <h1> MONGODB CONNECTED!!</h1>
-    `);
-});
 
-app.listen(PORT, () => {
-    console.log(`App listening on Port ${PORT}`);
-});
+connectDB()
+    .then(() => {
+    app.listen(PORT, () => {
+        console.log(`App listening on Port ${PORT}`);
+    });
+    })
+    .catch((error) => {
+    console.log("MongoDB connection failed !!!", error);
+    process.exit(1);
+    });
