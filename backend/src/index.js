139b1509a430a352f3a35143b83d177feabae7f7
Suraj Kumar <maybesurajkumar@gmail.com>
2026-07-21 14:26:14 +0530

    Exprress added succesfully server started

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 3000
+app.get("/",(req,res) =>{
+    res.send(`
+        <h1> Hello World</h1>
+        ${process.env.PORT}
+        `);
+})
+
+app.listen(PORT, () => {
+    console.log(`App listening on Port${PORT}`);
+    
+})
